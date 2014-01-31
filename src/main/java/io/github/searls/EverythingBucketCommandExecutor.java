2a98330116c6f2d20e4271fcbcc2044efd701a88
Justin Searls <searls@gmail.com>
2014-01-31 12:03:52 -0500

    intentionally *defactor* the code so I can TDD it
    Going to use this as an example repo for practicing GOOS-TDD against
    a third party framework API.

--- src/main/java/io/github/searls/EverythingBucketCommandExecutor.java
+++ src/main/java/io/github/searls/EverythingBucketCommandExecutor.java
@@ -1,13 +1,14 @@
 package io.github.searls;
 
-import io.github.searls.commands.RandomCommand;
+import java.util.Random;
 
-import java.util.Arrays;
-
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class EverythingBucketCommandExecutor implements CommandExecutor {
 
@@ -16,7 +17,17 @@ public class EverythingBucketCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player && command.getName().equalsIgnoreCase("random")) {
-			new RandomCommand().run((Player) sender, Arrays.asList(args));
+			Player player = (Player) sender;
+			Location target = player.getTargetBlock(null, 200).getLocation();
+
+			Material randomMaterial;
+			do {
+				Material[] materials = Material.values();
+				randomMaterial = materials[new Random().nextInt(materials.length)];
+			} while(randomMaterial.isBlock());
+
+			player.getWorld().dropItem(target, new ItemStack(randomMaterial));
+			player.sendMessage("Have a random "+ randomMaterial.name());
 			return true;
 		} else {
 			return false;
